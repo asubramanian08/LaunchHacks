0f7591e7c9ff6e5fae384c1fe77db21c8cb2eb6d
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2022-06-04 15:58:55 -0400

    buy.py: selectBuys for one month

--- buy.py
+++ buy.py
@@ -1,46 +1,47 @@
-# picks what item to buy to maximize happiness
+# pick what items to buy to maximize happiness
 
-def selectBuys(monthlyBudget, timeSpan, monthlyItemCosts, monthlyItemValues,
-               oneTimeItemCosts, oneTimeItemValues):
+def selectBuys(budget, monthlyWantsPrices, monthlyWantsValues,
+               oneTimeWantsPrices, oneTimeWantsValues):
+    # determine the best items to buy (knapsack algorithm)
+    cumulativePrices = monthlyWantsPrices + oneTimeWantsPrices
+    cumulativeValues = monthlyWantsValues + oneTimeWantsValues
+    allPurchases, happiness = knapsack(
+        cumulativePrices, cumulativeValues, budget)
+
+    # determine which monthly vs. one time items were bought
     monthlyPurchases = []
     oneTimePurchases = []
-    happinessValues = []
-    cumulativeCosts = monthlyItemCosts + oneTimeItemCosts
-    cumulativeValues = monthlyItemValues + oneTimeItemValues
-    mthItmCount = len(monthlyItemCosts)
-    for i in range(timeSpan):
-        purchases, happiness = knapsack(cumulativeCosts, cumulativeValues)
-        happinessValues.append(happiness)
-        monthlyPurchases.append([])
-        oneTimePurchases.append([])
-        for j in purchases:
-            if j < mthItmCount:
-                monthlyPurchases[i].append(j)
-            else:
-                oneTimePurchases[i].append(j)
-                # stop further uses of the item
-                cumulativeValues[j] = 0
-    return monthlyPurchases, oneTimePurchases, happinessValues
+    mwCount = len(monthlyWantsPrice)
+    for itemBought in allPurchases:
+        # the item is within the range of monthly wants
+        (monthlyPurchases if itemBought <
+         mwCount else oneTimePurchases).append(itemBought)
+
+    # return all monthly and one time purchases made
+    # along with how happy it will make the user
+    return monthlyPurchases, oneTimePurchases, happiness
 
 
-def knapsack(costs, values, budget):
-    # run the knapsack
+def knapsack(prices, values, budget):
+    # run the knapsack algorithm
     knapsackMat = [[]]
     for i in range(budget):
         knapsackMat[0].append(0)
-    for i in range(1, len(costs) + 1):
+    for i in range(1, len(prices) + 1):
         knapsackMat.append([])
         for j in range(budget + 1):
             usingItemI = knapsackMat[i][j]
-            if j >= costs[i]:
-                usingItemI = knapsackMat[i][j - costs[i]] + values[i]
+            if j >= prices[i]:
+                usingItemI = knapsackMat[i][j - prices[i]] + values[i]
             knapsackMat[i+1][j] = max(knapsackMat[i][j], usingItemI)
+
     # figure out what items to include
     including = []
     currSpent = budget
-    for i in range(len(costs), 0, -1):
+    for i in range(len(prices), 0, -1):
         if knapsackMat[i][currSpent] != knapsackMat[i-1][currSpent]:
-            currSpent -= costs[i - 1]
+            currSpent -= prices[i - 1]
             including.append(i - 1)
+
     # return the items the include, max happiness
-    return including, knapsackMat[len(costs)][budget]
+    return including, knapsackMat[len(prices)][budget]
