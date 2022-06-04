d8e6645e397f4869cd608ae98c45f4215829e4ae
Jasamarbir <83849657+Jas7777@users.noreply.github.com>
2022-06-04 20:03:08 +0000

    Main.py ask user

--- main.py
+++ main.py
@@ -1,24 +1,70 @@
-def calculateSpendings(costs, income) -> int:
-  # get the user input
-  # I did (line 45-62)
+income = int(input("Enter your income per month: "))
+monthlyBudget = income
+
+def askUser(questions):
+  #questions=[rent, longterm_goal, food, gas_electricty_bill, gas]
+  totalCost = 0
+  for q in questions:
+    currCost = int(input(q))
+    totalCost += currCost
+  return totalCost
+
+# monthly costs
+print("Please enter your monthly cost for the following items")
+montlyCosts = askUser(["Rent/Morgage: ", "Food: ", "Gas: ", "Electicty bill: "])
   
+  # rent = int(input(questions))
+  # longterm_goal = int(input(questions))
+  # food = int(input(questions))
+  # gas_electricty_bill = int(input(questions))
+  # gas = int(input(questions)
+#monthly costs
+
+# number
+
+of elements as input
+
+for q in questions:
+  cost = int(input(q))
+  monthlyBudget = monthlyBudget - cost
+for i in range(4):
+  print(questions)
+
+# monthly wants
+itemPrices = [] # 
+
+n = int(input("type in the umber of the amount of items : "))
+  
+# iterating till the range
+for i in range(0, n):
+    ele = int(input())
+  
+    itemPrices.append(ele) # adding the element
+      
+print(lst)
+
+itemValues = [] # happiness value
 
 
 
-       self.testQuestionArray.append(
-           "What is your income"
-       )
-       self.testQuestionArray.append(
-           "One long term goal and the cost"
-       )
-       self.testQuestionArray.append(
-           "What is your age"
-       )
-       self.testQuestionArray.append(
-           "When do you want to retire"
-       )
-          
-       self.testQuestionArray.append(
-           "How much
- 
-# this file will just take the input given by @Chandu, call the functions, and send back the output to @Chandu
+# MAJOR LOOP
+for m in nextMonth:
+  
+  # ask for one time costs
+  # ask for one time wants
+    # price + happynes
+  # STOCKS
+
+#rent = int(input("Enter your rent: "))
+#longterm_goal = int(input("Enter the amount of money for your longterm #unneededgoal: "))
+#food = int(input("Enter your daily food cost: "))
+#gas_electricty_bill = int(input("Enter your monthly electricity/gas bill: "))
+#gas = int(input("How many times a month do you fll up gas: "))time=retirment-age
+#x=1000000/time
+#y=x/12
+#spending=income-rent-longterm_goal-30*food-gas_electricty_bill-gas*7-y
+#spendings=0.0725*spending
+#h=spendings-900
+#print(h)
+#if(h<0):
+  #print("hi")
